176716a3948407344dd0a87262aa6e13247e16ea
Timur Podnebesyy <timurpodnebesny@gmail.com>
2024-07-16 20:48:12 +0600

    Конечный вариант

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Введите строки в массив
+        Console.Write("Введите строки через запятую: ");
+        string input = Console.ReadLine();
+        string[] originalArray = input.Split(',');
+
+        // Фильтруем строки
+        string[] filteredArray = FilterStrings(originalArray);
+
+        // Выводим результат
+        Console.WriteLine("Отфильтрованный массив:");
+        foreach (string str in filteredArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    static string[] FilterStrings(string[] inputArray)
+    {
+        int count = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index] = str;
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
